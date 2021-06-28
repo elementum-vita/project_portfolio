542060f91136f43db20b00faaf837d044ed823e1
elementum-vita <82609134+elementum-vita@users.noreply.github.com>
2021-06-28 19:45:20 -0400

    Add files via upload
    2021-06-28, update from home

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ const Navigation = () => {
         <div className="sidebar">
             <div className="id">
                 <div className="idContent">
-                    <img src="./media/me.png" alt="profile-img"/>
+                    <img src="./media/roxanne.png" alt="profile-img"/>
                     <h3>Roxanne Labelle-Lecompte</h3>
                 </div>
             </div>
@@ -15,11 +15,47 @@ const Navigation = () => {
 
             <div className="navigation">
                 <ul>
-                    <NavLink exact to="/" activeClassName="navActive">
-                        <i className="fas fa-home"></i>
-                        <span>Accueil</span>
-                    </NavLink>
+                    <li>
+                        <NavLink exact to="/" activeClassName="navActive">
+                            <i className="fa fa-home"></i>
+                            <span> Accueil</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/competences" activeClassName="navActive">
+                            <i className="fa fa-sliders"></i>
+                            <span> Compétences</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/portfolio" activeClassName="navActive">
+                            <i className="fa fa-file-code-o"></i>
+                            <span> Portfolio</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/contact" activeClassName="navActive">
+                            <i className="fa fa-envelope-o"></i>
+                            <span> Contact</span>
+                        </NavLink>
+                    </li>
                 </ul>
+            </div>
+
+            <div className="socialNetworks">
+                <ul>
+                    <li>
+                        <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+                            <i className="fa fa-linkedin"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+                            <i className="fa fa-github"></i></a>
+                    </li>
+                </ul>
+                <div className="signature">
+                    <p>Roxanne LabLec - 2021</p>
+                </div>
             </div>
         </div>
     
