f95842eba938fc254c3fe617bcb32ad2c5c02f19
elementum-vita <82609134+elementum-vita@users.noreply.github.com>
2021-06-28 19:45:51 -0400

    Add files via upload
    2021-06-28, update from home

---
+++ src/pages/Knowledges.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Knowledges = () => {
+    return (
+        <div>
+            <Navigation />
+            Pages sur les compétences / connaissances
+        </div>
+    );
+};
+
+export default Knowledges;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const NotFound = () => {
+    return (
+        <div>
+            <Navigation />
+            404
+        </div>
+    );
+};
+
+export default NotFound;
